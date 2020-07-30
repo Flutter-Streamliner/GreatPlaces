98b80dd0536e37cefc77b2d70803ee376bdbfb11
thisisstreamliner <thisisstreamliner@gmail.com>
2020-07-30 13:00:26 +0300

    update sdk. Storing image on the filesystem

--- lib/iwdgets/image_input.dart
+++ lib/iwdgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as pathProvider;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -15,6 +17,12 @@ class _ImageInputState extends State<ImageInput> {
     final picker = ImagePicker();
     final imageFile =
         await picker.getImage(source: ImageSource.camera, maxWidth: 600);
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    final appDir = await pathProvider.getApplicationDocumentsDirectory();
+    final fileName = path.basename(_storedImage.path);
+    final savedImage = await _storedImage.copy('${appDir.path}/$fileName');
   }
 
   @override
